cacebf0678baa4f4b83780c045b5e00ae3216856
KEDx7C1 <X7c1@yandex.ru>
2021-03-24 16:19:29 +0400

    GetWebPage method was created

---
+++ HtmlUniqueWords/Core/WebPage.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Net;
+
+namespace HtmlUniqueWords.Core
+{
+    /// <summary>
+    /// The Web Resorce class
+    /// Conteins methods for getting web page fron Internet to local drive
+    /// </summary>
+    class WebPage
+    {
+        public string Source { get; }
+        public string LocalFile { get; }
+
+               
+        public WebPage(string source, string localFile)
+        {
+            this.Source = source;
+            this.LocalFile = localFile;
+        }
+
+        /// <summary>
+        /// Get web page from internet to local drive
+        /// </summary>
+        public void GetPage()
+        {
+            //Create web request
+            WebRequest request = HttpWebRequest.Create(Source);
+            request.Method = "GET";
+                        
+            string line;
+            
+            //use StreamReader to get WebPage by lines
+            //if WabPabe will be very large, it will be downloaded in any way
+            using (StreamReader reader = new StreamReader(request.GetResponse().GetResponseStream()))
+            using (StreamWriter writer = new StreamWriter(LocalFile))
+            {
+                do
+                {
+                    line = reader.ReadLine();
+                    writer.WriteLine(line);
+                }
+                while (line != null);
+            }
+
+
+        }
+    }
+}
---
+++ HtmlUniqueWords/HtmlUniqueWords.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HtmlUniqueWords
+{
+    class HtmlUniqueWords
+    {
+        static void Main(string[] args)
+        {
+            Core.WebPage webPage = new Core.WebPage(args[0], args[1]);
+            try
+            {
+                webPage.GetPage();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
